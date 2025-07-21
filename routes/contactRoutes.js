739deb1420cc6b8c3f357bb9829a739152eff4ce
LucasAPaiva <paivalucas8456@gmail.com>
2025-07-21 15:11:52 -0300

    UPDATE

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,34 +1,13 @@
-
 const express = require('express');
 const router = express.Router();
-const Contact = require('../models/contact');
+const contact = require('../controllers/contactController');
 
-
-router.post('/', async (req, res) => {
-  try {
-    const { name, email, message } = req.body;
-
-    if (!name || !email || !message) {
-      return res.status(400).json({ error: 'Todos os campos são obrigatórios.' });
-    }
-
-    const newContact = await Contact.create({ name, email, message });
-    res.status(201).json(newContact);
-  } catch (error) {
-    console.error('Erro ao criar contato:', error);
-    res.status(500).json({ error: 'Erro interno do servidor.' });
-  }
-});
-
-
-router.get('/', async (_req, res) => {
-  try {
-    const contacts = await Contact.findAll();
-    res.json(contacts);
-  } catch (error) {
-    console.error('Erro ao buscar contatos:', error);
-    res.status(500).json({ error: 'Erro interno do servidor.' });
-  }
-});
-
+ 
+// Criar um novo contato
+router.post('/', contact.createContact);
+ 
+// Listar todos os contatos
+router.get('/', contact.listContact);
+ 
 module.exports = router;
+ 
